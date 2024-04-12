ea693dd6bdab0c866e735bd7b4cd4c65a57311b3
NilsBach <155982682+NilsBach@users.noreply.github.com>
2024-04-12 08:15:29 +0000

    tried routing with youtube tutorial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import './AppOutput.css';
 import { Navbar } from "flowbite-react";
-
+import Page1 from './routes/Page1';
+import Page2 from './routes/Page2';
+import Page3 from './routes/Page3';
+import { Route, Routes, Link} from 'react-router-dom';
 
 function App() {
   return (
@@ -9,7 +12,12 @@ function App() {
        {navbar()}
       </div>
       <div>
-        <p> Das hier ist mal so ein Test</p>
+           <Routes>
+           <Route path="/" element={<Home />} />
+              <Route path="/Page1" element={<Page1 />} />
+              <Route path="/Page2" element={<Page2 />} />
+              <Route path="/Page3" element={<Page3 />} />
+            </Routes>
       </div>  
     </div>
   );
@@ -22,15 +30,17 @@ function navbar(){
               <span className="self-center whitespace-nowrap text-xl font-semibold">Flowbite React</span>
             </Navbar.Brand>
             <Navbar.Collapse>
-              <Navbar.Link href="/" active>
+              <Navbar.Link as={Link} href="/" active>
                 Home
               </Navbar.Link>
               <Navbar.Link href="/page1">YOPE</Navbar.Link>
               <Navbar.Link href="/page2">Services</Navbar.Link>
-              <Navbar.Link href="#">Pricing</Navbar.Link>
+              <Navbar.Link href="/page3">Pricing</Navbar.Link>
               <Navbar.Link href="#">Contact</Navbar.Link>
             </Navbar.Collapse>
           </Navbar>
+              
+
         );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Page1 from './routes/Page1';
-import Page2 from './routes/Page2';
-import Page3 from './routes/Page3';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/Page1" element={<Page1 />} />
-        <Route path="/Page2" element={<Page2 />} />
-        <Route path="/Page2" element={<Page2 />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
