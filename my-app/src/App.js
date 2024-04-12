5849f4256ebf558c32d03223a071524d72b65597
NilsBach <155982682+NilsBach@users.noreply.github.com>
2024-04-12 06:18:03 +0000

    started with layout and experimenting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,45 @@
-import logo from './logo.svg';
 import './AppOutput.css';
-import { Button } from "flowbite-react";
-import { Alert } from "flowbite-react";
-
-
+import { Navbar } from "flowbite-react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
 
 function App() {
   return (
-    <div>
-    <p className='text-red-900'> Hallo Test Seite :) </p>
-    <div className="flex flex-wrap gap-2">
-      <Button onClick={function handleClick(){
-            return(
-               <Alert color="info">
-                <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-              </Alert>
-            );
-      } }>Click me</Button>
-    </div>    <div>
-      <p> TEST</p>
-    </div>
+    <div id="container">
+      {/* adding the navbar */}
+      <div id="navBar" className="bg-stone-800">
+        { navbar() }
+      </div>
+      {/* adding the main page*/}
+      <div id ="main" className="bg-stone-900 text-white">
+        <p> test2</p>
+      </div>
     </div>
 
   );
 }
-
-
+function navbar(){
+    return(
+          <Navbar fluid rounded className= "text-white bg-stone-800">
+            <Navbar.Brand href="https://flowbite-react.com">
+              <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Cross" />
+              <span className="self-center whitespace-nowrap text-xl font-semibold">Flowbite React</span>
+            </Navbar.Brand>
+            <Navbar.Collapse>
+              <Navbar.Link href="#" active>
+                Home
+              </Navbar.Link>
+              <Navbar.Link href="#">YOPE</Navbar.Link>
+              <Navbar.Link href="#">Services</Navbar.Link>
+              <Navbar.Link href="#">Pricing</Navbar.Link>
+              <Navbar.Link href="#">Contact</Navbar.Link>
+            </Navbar.Collapse>
+          </Navbar>
+        );
+}
 
 export default App;
---
+++ my-app/src/components/layout/Layout.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./Navigation";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navigation />
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
